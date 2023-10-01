e8cb60dc7cc0f18c4f1578eb769bf609aa2ce394
Kausthub Kannan <kausthubkannan961@gmail.com>
2023-10-01 11:52:19 +0000

    meme api and commands added

--- bot_reply.py
+++ bot_reply.py
@@ -1,15 +1,19 @@
-import json
-import requests
-import random
 import os
 from dotenv import load_dotenv
+import random
+import json
+import requests
+import urllib.request
 
+# Get the Hugging face Token
 load_dotenv()
 API_TOKEN = os.getenv("API_TOKEN")
 
+# Set params for the request
 API_URL = "https://api-inference.huggingface.co/models/pranavpsv/gpt2-genre-story-generator"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
+# Stroy generation function
 def story_generator(input_text):
     payload ={
         "inputs": input_text,
@@ -20,3 +24,9 @@ def story_generator(input_text):
     output = json.loads(response.content.decode("utf-8"))
     output = output[0]["generated_text"]
     return output
+
+# Meme generation function
+async def get_meme():
+    content = requests.get("https://api.pymeme.repl.co").text
+    data = json.loads(content)
+    return data
--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-import discord
-import os
 from dotenv import load_dotenv
-from bot_reply import story_generator
+import os
 from datetime import datetime
+import discord
+from discord.ext import commands
+from discord import app_commands
+from bot_reply import story_generator, get_meme
 
+
+# Load the Bot Token
 load_dotenv()
-
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
+
+# Specify the permissions
 intents = discord.Intents.default()
 intents.message_content = True
-bot = discord.Client(intents=intents)
 
 
+# Create the bot
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+# Sync commands
 @bot.event
 async def on_ready():
     guild_count = 0
@@ -21,23 +30,43 @@ async def on_ready():
     for guild in bot.guilds:
         guild_count = guild_count + 1
 
+    try:
+        synced = await bot.tree.sync()
+        print("Commands synced")
+    except Exception as e:
+        print(e)
+
     print("The bot has started! Currenlty working in " + str(guild_count) + " guild.")
 
 
-@bot.event
-async def on_message(message):
-    msg = message.content
+# Story Generation
+@bot.tree.command(name="story", description="story generation")
+@app_commands.describe(text = "The story of")
+async def send_story(interaction: discord.Interaction, text: str):
 
-    if msg.startswith("<@1157375972908212294>"):
-        # Clean the content before generation
-        msg = msg.replace("<@1157375972908212294>", "")
+    # Perform the generation and send the message
+    reply = story_generator(text)
+    
+    # LOG
+    log = "LOG ["+ str(datetime.now()) +"]: STATUS [200]: Message [Story generated]"
+    print(log)
 
-        # Send a wait message
-        await message.channel.send("Cooking your story.....")
+    await interaction.response.send_message(reply)
 
-        # Perform the generation and send the message
-        reply = story_generator(msg) + "\nBOOM! Next time let's continue"
-        await message.channel.send(reply)
-        print("LOG ["+ str(datetime.now()) +"]: STATUS [200]: Message [Story generated]")
+
+# Meme Generation Command
+@bot.tree.command(name="meme", description="random meme")
+async def send_meme(interaction: discord.Interaction):
+
+    # Get meme data from API
+    data = await get_meme()
+    meme = data["meme"]
+
+    # LOG
+    log = "LOG ["+ str(datetime.now()) +"]: STATUS [200]: Message [Meme generated]"
+    print(log)
+
+    # Reply to the user and send the meme
+    await interaction.response.send_message(meme["image url"])
 
 bot.run(DISCORD_TOKEN)
